39592654e3bcae4de7ef0b72b4def0696eda52ce
Marc Fargas <marc@marcfargas.com>
2026-09-23 10:17:52 +0200

    fix: verify compiled MSI service tables

--- src/installer/msi/verify.ps1
+++ src/installer/msi/verify.ps1
@@ -29,8 +29,9 @@ function Query([string]$Sql) {
     return $rows
 }
 function Field($Record, [int]$Index) { $Record.GetType().InvokeMember('StringData', 'GetProperty', $null, $Record, @($Index)) }
+function LongFileName([string]$Value) { return @($Value -split '\|')[-1] }
 
-$files = @(Query 'SELECT `FileName`,`Component_` FROM `File`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)" })
+$files = @(Query 'SELECT `FileName`,`Component_` FROM `File`' | ForEach-Object { "$(LongFileName (Field $_ 1))|$(Field $_ 2)" })
 if (@($files | Where-Object { $_ -match '^go-mapi\.dll\|' }).Count -ne 2) { Fail 'MSI must contain exactly two interceptor DLL files' }
 if (@($files | Where-Object { $_ -match '^go-mapi-service\.exe\|ResidentService$' }).Count -ne 1) { Fail 'MSI must contain exactly one resident service executable' }
 if ($files -match '(?i)wails|webview') { Fail 'system MSI contains forbidden user-app payload' }
@@ -40,13 +41,13 @@ Query 'SELECT `Property`,`Value` FROM `Property`' | ForEach-Object { $properties
 if ($properties.ProductCode.Trim('{}') -ne $identity.productCode -or $properties.ProductVersion -ne $identity.productVersion -or $properties.UpgradeCode.Trim('{}') -ne 'B3C97B33-3F10-47CA-9FA7-24EE3B75E325') { Fail 'compiled MSI identity does not match the production system identity' }
 
 $services = @(Query 'SELECT `Name`,`DisplayName`,`ServiceType`,`StartType`,`ErrorControl`,`StartName`,`Arguments`,`Component_` FROM `ServiceInstall`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)|$(Field $_ 3)|$(Field $_ 4)|$(Field $_ 5)|$(Field $_ 6)|$(Field $_ 7)|$(Field $_ 8)" })
-if ($services.Count -ne 1 -or $services[0] -ne 'go-mapi|go-mapi system service|16|2|1|LocalSystem|service|ResidentService') { Fail "unexpected resident service contract: $($services -join ';')" }
+if ($services.Count -ne 1 -or $services[0] -ne 'go-mapi|go-mapi system service|16|2|32769|LocalSystem|service|ResidentService') { Fail "unexpected resident service contract: $($services -join ';')" }
 $controls = @(Query 'SELECT `Name`,`Event`,`Wait`,`Component_` FROM `ServiceControl`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)|$(Field $_ 3)|$(Field $_ 4)" })
-if ($controls.Count -ne 1 -or $controls[0] -ne 'go-mapi|305|1|ResidentService') { Fail "unexpected ServiceControl contract: $($controls -join ';')" }
+if ($controls.Count -ne 1 -or $controls[0] -ne 'go-mapi|163|1|ResidentService') { Fail "unexpected ServiceControl contract: $($controls -join ';')" }
 $serviceConfig = @(Query 'SELECT `Name`,`Event`,`ConfigType`,`Argument`,`Component_` FROM `MsiServiceConfig`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)|$(Field $_ 3)|$(Field $_ 4)|$(Field $_ 5)" })
-if ($serviceConfig.Count -ne 2 -or $serviceConfig -notcontains 'go-mapi|3|3|1|ResidentService' -or $serviceConfig -notcontains 'go-mapi|3|5|1|ResidentService') { Fail "service is not delayed-auto-start with an unrestricted SID: $($serviceConfig -join ';')" }
+if ($serviceConfig.Count -ne 2 -or $serviceConfig -notcontains 'go-mapi|5|3|1|ResidentService' -or $serviceConfig -notcontains 'go-mapi|5|5|1|ResidentService') { Fail "service is not delayed-auto-start with an unrestricted SID: $($serviceConfig -join ';')" }
 $failureActions = @(Query 'SELECT `Name`,`Event`,`ResetPeriod`,`Actions`,`DelayActions`,`Component_` FROM `MsiServiceConfigFailureActions`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)|$(Field $_ 3)|$(Field $_ 4)|$(Field $_ 5)|$(Field $_ 6)" })
-if ($failureActions.Count -ne 1 -or $failureActions[0] -notmatch '^go-mapi\|3\|86400\|1\[~\]1\[~\]0\[~\]\|60000\[~\]300000\[~\]0\[~\]\|ResidentService$') { Fail "service failure actions are not bounded restart/restart/none: $($failureActions -join ';')" }
+if ($failureActions.Count -ne 1 -or $failureActions[0] -ne 'go-mapi|5|86400|1[~]1[~]0|60000[~]300000[~]0|ResidentService') { Fail "service failure actions are not bounded restart/restart/none: $($failureActions -join ';')" }
 
 $registry = @(Query 'SELECT `Root`,`Key`,`Name`,`Value`,`Component_` FROM `Registry`' | ForEach-Object { "$(Field $_ 1)|$(Field $_ 2)|$(Field $_ 3)|$(Field $_ 4)|$(Field $_ 5)" })
 if (@($registry | Where-Object { $_ -match 'MapiRegistrationShared' }).Count -lt 3) { Fail 'missing shared active-MAPI registry rows' }
